a05084eb65707e818c30d1b4e3c67c742ce5f629
ixnp <enprocopios@gmail.com>
2019-09-13 14:19:28 -0700

    styled nav

---
+++ portfolio/src/components/Resume.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../scss/main.scss';
+
+//TODO build out resume and add a pdf version with dropbox
+//Link to this page with react router. It should be its own thing
+const Resume = () => {
+    return (
+      <div className="Resume">
+        <div className="header"></div>
+        <div className="contact"></div>
+        <div className="experince"></div>
+        <div className="skills"></div>
+        <div className="school"></div>
+        <div className="downloadPDF"></div>
+      </div>
+    );
+  }
+
+export default Resume
