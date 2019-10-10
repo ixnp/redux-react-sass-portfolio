797ce8a1a8cca5aea075d197817ec3fd4e6a06bb
ixnp <enprocopios@gmail.com>
2019-10-09 18:00:37 -0700

    fixing some size issues

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -10,10 +10,9 @@ const Header = () => {
           <div className="header-container-text">
           <h1>Ix Procopios</h1>
           <h2>Software Developer</h2>
-          <h2>Designer</h2>
-          <h2>Artist</h2>
+        
           </div>
-      <img className="header-container-img overlay"src={bannerImage}></img>
+        <img className="header-container-img overlay" src="https://lh3.googleusercontent.com/8X0BBRz1vUSC4SoO8YacFJX2_bbtcxgVGsNkBtjTG39gcF9V82d1xyofl2p128XmT2ZNsfQFU8mRo-a-eDMhxCTmM0ptneyBQXZMS05a_xAN6lmE0BNEelKRBGub7U6iaE2uQyYEtID-iOYMRfyWVAtEreM0PhhQL1LfUEJDskZLCrAkid25bE9VaSWaZpGe4DS2j_yR4TCSltMUEbt9DuY92W3wZ8oLUduDgb8ATnChYntuRkSms81htXM86SmXq7z0QyczfEAD6Im5BllvFwC2dxmlxA3N4K61CR3bM9h4bZZOFzqOkUwn1SYhWl5wsU3Ob-NMNQ-SGtrW7OaFRgueu__bcRxRlMjB88zZKZ-nknA97AlUkdrIAGyFbnp0mcO0Jkgvw8-65laqUdSp3jvjKi7XB3twPRp5YByuhnddLd7GphTjxSrZvmjKFZu34cv5SzMU8UTRbmpIlgvnoncybL66JD-rZbD4vgUw42zIu0lr7uqC7T0whF53iR2_70_ULq66hh_RC_JejcPj6JVwuOp26aB3i7BQel2bwqCr5LRUlTPODAgXGyVXARUC_CycjOIAmx_Kd9KB7yTgc2Zy8qtLUNJVh93sPUKGbriXjH_R6TSEcZcJxZ_u8jnes6pC9g159bfaFb7pJdAoGQSUPfeJVk8Mb-lB7Lq8Tg7sQ46hHCn6uuMQOfefCr1FkKwvmHyhCSyenYqDC75ZRGZLwyC65voSr48OFR1R8DKXr29Y=w1462-h890-no"></img>
       <Nav></Nav>
       <Menu></Menu>
       </div>
--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -39,7 +39,7 @@ class Skills  extends React.Component{
         <div className="skills-info">
       
         <div className="circle">
-          <img className="avatar" src={avatar}></img>
+              <img className="avatar" src="https://lh3.googleusercontent.com/j4adCneorPBKAwdc6OpFaWEgXRIFMko1ifOqCZHtTN0arepFJUXY_fwO9Xe4XkUpozdAzKs2aRVioUSkN0lm99U60T3RNLlsgc1cYV3IxvpqnMk3qwsAujbbpJUnoc4NbRuTqTi4_huSPJxOCbqcG-NtT5STHDDfxvNV1P8SoXqaLdyPn-mF-YuwY1R9aaTfpEaVSqEkJgF24xoqeoYOUM_sDu8dKadQ3EQ0HA6S_K43F3Qi8Sh5T-WTWP7aMezv3jsYhlYeLp3tITLdfH0N8DdutkxMnzyYmaP5cLDdW8-xGALWSQPlmeAJZA4ZwEVpSy_Ft6wFVjqdOIyRM-7eSlzUDUTvRBwpc_iE3iK0hwsOjZeki09lqALri0gckzttNpZH2ej3W0Q97qvc28Kw5q3yJDcV1npJFKY2Koe5nPo5W66imnGmqunjiNcHv2YCNQqjS_SQLF8-dFPmoH_ReNmqOYLgYk5GBPUZmVKVaz6nFu8gU3WgX3oCe_iDmegpKuFPuXY06Gxrl2ayNkalzsDJniQVx-zOeMdPOKy8MBcTjZ3wPh5AvzKiJabAiNRMaWv6G1Y49iB___wjiJp_pG4ABXmR_SBAR6j0gHqh7-mNmaFc7jTFVPMwNBbInbtkNHxk8bravw2zmKSdfEIZKeLkr7NFFdR0kVd3fcASjSK9ZXoOEdBDKSBh-P6MfgculA8hW4dSJGY8flL9EnRzMTQlOCTRvH-29PrDi5Ilo2DVrT6t=w668-h890-no"></img>
           </div>
           <div className="skills-info-text">
           <h3>Who Am I?</h3>
