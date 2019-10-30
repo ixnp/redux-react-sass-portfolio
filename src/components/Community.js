8429a4a0ff2ee37620e539f88a0909962c8113b3
ixnp <enprocopios@gmail.com>
2019-10-30 16:08:22 -0700

    added community component

---
+++ src/components/Community.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/main.scss';
+
+class Community extends React.Component {
+
+    render(){
+        return(
+            <div>Community</div>
+        )
+    }
+}
+
+export default Community
